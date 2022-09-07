3052979b60e50664b3a3f4b4c39b3278d81d5c11
sitb157 <108820413+sitb157@users.noreply.github.com>
2022-09-07 12:37:21 +0900

    Time: 192 ms (5.01%), Space: 14.5 MB (26.00%) - LeetHub

--- 99-recover-binary-search-tree/99-recover-binary-search-tree.py
+++ 99-recover-binary-search-tree/99-recover-binary-search-tree.py
@@ -5,29 +5,30 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def rR(self, max_t, min_t, c_t, max_, min_, root):
-        if (min_ != None) and (c_t.val <= min_):
-            min_t.val, c_t.val = c_t.val, min_t.val
-            r_t = root
-            self.rR(None, None, r_t, None, None, root)
-            return None
-        if (max_ != None) and (c_t.val >= max_):
-            max_t.val, c_t.val = c_t.val, max_t.val
-            r_t = root
-            self.rR(None, None, r_t, None, None, root)
-            return None
+    def rR(self, max_t, min_t, c_t, max_, min_, s_l):
         if c_t.left != None:
             t_t = c_t
-            self.rR(t_t, min_t, c_t.left, c_t.val, min_, root)
+            s_l = self.rR(t_t, min_t, c_t.left, c_t.val, min_, s_l)
+        if (min_ != None) and (c_t.val <= min_):
+            s_l.append([min_t, c_t])
+        if (max_ != None) and (c_t.val >= max_):
+            s_l.append([c_t, max_t])
         if c_t.right != None:
             t_t = c_t
-            self.rR(max_t, t_t, c_t.right, max_, c_t.val, root)
-        return None
+            s_l = self.rR(max_t, t_t, c_t.right, max_, c_t.val, s_l)
+        return s_l
     def recoverTree(self, root: Optional[TreeNode]) -> None:
         """
         Do not return anything, modify root in-place instead.
         """
         print(root)
         c_t = root
-        self.rR(None, None, c_t, None, None, root)
+        s_l = []
+        s_l = self.rR(None, None, c_t, None, None, s_l)
+        s1 = s_l[0][0]
+        s2 = s_l[-1][1]
+        s1.val, s2.val = s2.val, s1.val
+            
+            
+            
         
